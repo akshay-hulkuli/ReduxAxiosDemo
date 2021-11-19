366e12ca579a7138a6fd4bc7fae5138b488914e3
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-19 21:56:33 +0530

    [Akshay]ADDED: redux reducer to perform login

---
+++ src/reducers/reducer.js
@@ -0,0 +1,11 @@
+export const loginReducer = (state=0, action) => {
+    switch(action.type){
+        case'LOGIN': {
+            return state+1;
+        }
+        case'LOGOUT': {
+            return state-1;
+        }
+        default : return state;
+    }
+}

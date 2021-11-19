50e11742e769440120908240e56fb550044ab17c
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-19 21:42:08 +0530

    [Akshay]ADDED: redux store in index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Route,Routes,BrowserRouter as Router} from 'react-router-dom'
+import {useSelector} from 'react-redux';
+import Login from './components/Login';
+import Home from './components/Home';
 function App() {
+  var loginStatus = useSelector(state => state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is a react-redux example project</h1>
+      <p id='login-indicator'>are you logged in : {loginStatus}</p>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+          <Route exact path='/home' element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }

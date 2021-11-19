188692bbd2dc2be2cbef8c7d815a93b6dc324fa8
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-19 21:40:25 +0530

    [Akshay]ADDED: redux store in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore,applyMiddleware} from 'redux';
+import { loginReducer } from './reducers/reducer';
+import { Provider } from 'react-redux';
+import thunk from "redux-thunk"
+
+const myStore = createStore( 
+  loginReducer, applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={myStore}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 

4575412f3615871885439a51861691d0b335aa50
Akshaya HP <akshayhulkuli@gmail.com>
2021-11-20 12:03:33 +0530

    [Akshay]REFACTORED: used compose to include dev tools in store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware, compose} from 'redux';
 import { loginReducer } from './reducers/reducer';
 import { Provider } from 'react-redux';
 import thunk from "redux-thunk"
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+  // other store enhancers if any
+);
+
 const myStore = createStore( 
-  loginReducer, applyMiddleware(thunk)
+  loginReducer, enhancer
 )
+
+// const myStore = createStore ( loginReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={myStore}>
